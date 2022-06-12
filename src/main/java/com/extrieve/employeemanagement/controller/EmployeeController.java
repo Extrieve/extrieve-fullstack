bfdb9befaa923b96c9946eefc12a93e6fea0da16
Nick <nicolas.cinera@gmail.com>
2022-06-12 15:50:57 -0500

    Support Angular Local Calls

--- src/main/java/com/extrieve/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/extrieve/employeemanagement/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.extrieve.employeemanagement.controller;
 import com.extrieve.employeemanagement.model.Employee;
 import com.extrieve.employeemanagement.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -16,12 +13,13 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/employees")
     public Collection<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/save")
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
